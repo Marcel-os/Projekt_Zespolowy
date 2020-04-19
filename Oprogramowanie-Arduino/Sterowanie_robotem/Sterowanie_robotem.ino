c50e8108c115f1498252e3b4f221ceeb28bbba3f
Marcel-os <marcel-domagala@wp.pl>
2020-04-20 00:15:49 +0200

    Dodano kilka funkcji

--- Oprogramowanie-Arduino/Sterowanie_robotem/Sterowanie_robotem.ino
+++ Oprogramowanie-Arduino/Sterowanie_robotem/Sterowanie_robotem.ino
@@ -1,18 +1,62 @@
-/*
-Programik do sterowania robotem, założenia:
--sterowanie PWM dwoma silnikami
--odczyt z czujnika odległości
--odczyt z czujnika podłoża
--wysyłanie i odbieranie danych UART
-*/
+#define PWM_L_PIN 5
+#define PWM_R_PIN 6
+
+#define MOTOR_L_PIN1 7
+#define MOTOR_L_PIN2 8
+
+#define MOTOR_R_PIN1 9
+#define MOTOR_R_PIN2 10
+
+#define DISTANCE_SENSOR_PIN A0
+#define GROUND_SENSOR1_PIN A1
+#define GROUND_SENSOR2_PIN A2
 
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(MOTOR_L_PIN1, OUTPUT);
+  pinMode(MOTOR_L_PIN2, OUTPUT);
+  pinMode(MOTOR_R_PIN1, OUTPUT);
+  pinMode(MOTOR_R_PIN2, OUTPUT);
+
+  digitalWrite(MOTOR_L_PIN1, LOW);
+  digitalWrite(MOTOR_L_PIN2, LOW);
+  digitalWrite(MOTOR_R_PIN1, LOW);
+  digitalWrite(MOTOR_R_PIN2, LOW);
+
+  analogWrite(PWM_L_PIN, 0);
+  analogWrite(PWM_R_PIN, 0);
 }
 
+void go_straight(byte PWM_Speed){
+  analogWrite(PWM_L_PIN, PWM_Speed);
+  analogWrite(PWM_R_PIN, PWM_Speed);
+
+  digitalWrite(MOTOR_L_PIN1, LOW);
+  digitalWrite(MOTOR_L_PIN2, HIGH);
+  digitalWrite(MOTOR_R_PIN1, LOW);
+  digitalWrite(MOTOR_R_PIN2, HIGH);
+}
+
+void go_back(byte PWM_Speed){
+  analogWrite(PWM_L_PIN, PWM_Speed);
+  analogWrite(PWM_R_PIN, PWM_Speed);
+
+  digitalWrite(MOTOR_L_PIN1, HIGH);
+  digitalWrite(MOTOR_L_PIN2, LOW);
+  digitalWrite(MOTOR_R_PIN1, HIGH);
+  digitalWrite(MOTOR_R_PIN2, LOW);
+}
+
+int get_distance(){
+  return analogRead(DISTANCE_SENSOR_PIN);
+}
+
+void get_distance_ground(int &distance1, int &distance2){
+  distance1 = analogRead(GROUND_SENSOR1_PIN);
+  distance2 = analogRead(GROUND_SENSOR2_PIN);
+}
+
+
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+  
+
 }
