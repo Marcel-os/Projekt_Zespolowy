11a89345d902af97bc0abfc8c269569095f26de3
dzeju <62911257+dzeju@users.noreply.github.com>
2020-05-19 12:11:16 +0200

    zmiana z xamarin na xamarin.forms

---
+++ Oprogramowanie-Android/App4/App4/ControlPage.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App4
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ControlPage : ContentPage
+    {
+        NetworkStream serverStream;
+        TcpClient clientSocket;
+
+        public ControlPage(NetworkStream sStream, TcpClient cSocket)
+        {
+            InitializeComponent();
+
+            serverStream = sStream;
+            clientSocket = cSocket;
+        }
+
+        private async void BtnSend_Clicked(object sender, EventArgs e)
+        {
+            if (clientSocket.Connected)
+            {
+                byte[] outstream = Encoding.ASCII.GetBytes("hej");
+
+                serverStream.Write(outstream, 0, outstream.Length);
+                serverStream.Flush();
+            }
+            else
+            {
+                await DisplayAlert("Error", "Not connected", "OK");
+                await Navigation.PopModalAsync();
+            }
+        }
+
+        private async void BtnDisconnect_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                clientSocket.Close();
+                serverStream.Close();
+                await DisplayAlert("Disconnected :c", "", "OK");
+                await Navigation.PopModalAsync();
+            }
+            catch { }
+        }
+
+        private async void Switch_Toggled(object sender, ToggledEventArgs e)
+        {
+            if (clientSocket.Connected)
+            {
+                byte[] outstream = null;
+                if (switchTg.IsToggled)
+                {
+                    outstream = Encoding.ASCII.GetBytes("auto");
+                    ControlButtons(true);
+                }
+                if (!switchTg.IsToggled)
+                {
+                    outstream = Encoding.ASCII.GetBytes("manual");
+                    ControlButtons(false);
+                }
+                serverStream.Write(outstream, 0, outstream.Length);
+                serverStream.Flush();
+            }
+            else
+            {
+                await DisplayAlert("Error", "Not connected", "OK");
+                await Navigation.PopModalAsync();
+            }
+        }
+
+        private void ControlButtons(bool auto)
+        {
+            if (!auto)
+            {
+                btnUp.IsVisible = true;
+                btnDown.IsVisible = true;
+                btnLeft.IsVisible = true;
+                btnRight.IsVisible = true;
+            }
+            if (auto)
+            {
+                btnUp.IsVisible = false;
+                btnDown.IsVisible = false;
+                btnLeft.IsVisible = false;
+                btnRight.IsVisible = false;
+            }
+        }
+    }
+}
---
+++ Oprogramowanie-Android/App4/App4/MainPage.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Sockets;
+using System.Threading;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App4
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        TcpClient clientSocket;
+        NetworkStream serverStream;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnAutofill_Clicked(object sender, EventArgs e)
+        {
+            txtIp.Text = "192.168.8.122";
+            txtPort.Text = "8080";
+        }
+
+        private async void BtnConnect_Clicked(object sender, EventArgs e)
+        {
+            string ip = txtIp.Text;
+            string port = txtPort.Text;
+
+            clientSocket = new TcpClient();
+            serverStream = default(NetworkStream);
+
+            try
+            {
+                clientSocket.Connect(ip, Int32.Parse(port));
+                Thread ctThread = new Thread(Connect);
+                ctThread.Start();
+                if (clientSocket.Connected)
+                {
+                    await DisplayAlert("Connected :D", "", "OK");
+                    await Navigation.PushModalAsync(new NavigationPage(new ControlPage(serverStream, clientSocket)));
+                }
+            }
+            catch (SocketException en)
+            {
+                await DisplayAlert("Error", en.Message, "OK");
+            }
+            catch (FormatException en)
+            {
+                await DisplayAlert("Error", en.Message, "OK");
+            }
+            catch (ArgumentNullException en)
+            {
+                await DisplayAlert("Error", en.Message, "OK");
+            }
+        }
+
+        private void Connect()
+        {
+            serverStream = clientSocket.GetStream();
+        }
+    }
+}
